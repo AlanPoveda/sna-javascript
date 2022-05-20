8367c1a9eae4187c507c80ebbafd98f4b4cca1f1
Alan Poveda <alsepoveda@gmail.com>
2022-05-20 12:28:10 -0300

    Finish lab 21

--- lab021/script.js
+++ lab021/script.js
@@ -1,15 +1,15 @@
 
+let cont = 1
+saida.innerHTML = `<h2>Contando de 1 até 10</h2>`
+
+//Tirei o if e ainda deixei algumas variáveis globais
 function contar() {
+
     let saida = document.getElementById('saida')
 
-    saida.innerHTML += `<h2>Contando de 1 até 10</h2>`
-
-    let cont = 1
     while (cont <= 10) {
         saida.innerHTML += ` ${cont} &#x1F449;`
-        if (x === 0) {
         cont ++ // Corresponde a cont = cont + 1
-        }
     }
     saida.innerHTML += ` &#x1F3C1;`
 }
