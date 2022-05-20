f754100621177416be6158633fdcf186ec7a822b
Alan Poveda <alsepoveda@gmail.com>
2022-05-20 14:33:25 -0300

    Create a final challenge, and make the 'desafio-boas-vindas', and insert the folders for the final challenge

---
+++ Desafio-final/desafio-boas-vindas/index.js
@@ -0,0 +1,9 @@
+// Função criada para receber o nome e a idade do usuário!
+
+
+function welcome(){
+    let name = window.prompt("Digite seu nome")
+    let age = window.prompt("Digite a sua idade")
+
+    alert(`Acabei de conhecer o ${name} que tem ${age} de idade`)
+}
