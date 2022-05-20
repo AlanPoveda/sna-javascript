f12e0dfb4ff297288a51494b7b5507f198905150
Alan Poveda <alsepoveda@gmail.com>
2022-05-20 11:40:49 -0300

    Finish lab 16

--- lab016/idade.js
+++ lab016/idade.js
@@ -1,11 +1,12 @@
-
-function calcidade{} (
+// Colocando parênteses e colocando chaves
+function calcidade() {
     let agora = new Date
     let ano = agora.getFullYear()
 
-    let nasc = Number(window.prompt('Em que mês você nasceu?'))
+    // E troquei para ano
+    let nasc = Number(window.prompt('Em que ano você nasceu?'))
     let idade = ano - nasc
 
     let saida = document.getElementById('saida')
     saida.innerHTML = `<p>Quem nasceu em ${nasc} vai completar <strong>${idade}</strong> anos em ${ano}.</p>`
-)
+}
