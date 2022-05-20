0f1a1563d5f4bb58beaee39207524d34e32f90e4
Alan Poveda <alsepoveda@gmail.com>
2022-05-20 11:33:15 -0300

    Finish lab 14

--- lab014/functions.js
+++ lab014/functions.js
@@ -1,5 +1,5 @@
-
-function info[] {
+// colocando parênteses
+function info() {
     let agora = new Date
     let saida = document.getElementById('saida')
     saida.innerHTML = `<p>O que eu recebi do sistema foi <mark>${agora}</mark></p>`
