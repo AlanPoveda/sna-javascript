0276650535ac9189fe79c15f4488b020b2fa0f8d
Alan Poveda <alsepoveda@gmail.com>
2022-05-20 11:19:20 -0300

    Finish lab 123

--- lab012/parimpar.js
+++ lab012/parimpar.js
@@ -2,12 +2,14 @@
 function teste() {
     let num = Number(window.prompt('Digite um número: '))
     let tipo
-    if (num % 2 == 1) {
+    // Troquei para == 0
+    if (num % 2 == 0) {
         tipo = '<strong>PAR</strong>'
     } else {
         tipo = '<strong>ÍMPAR</strong>'
     }
 
-    let res = querySelector('section#result')
+    // Coloquei o document para pegar o seletor corretamente.
+    let res = document.querySelector('section#result')
     res.innerHTML = `<p>O número ${num} que foi digitado é ${tipo}!</p>`
 }
