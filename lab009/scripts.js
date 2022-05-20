c66de3e09b2f2cb1b1d9091c3bd4799f869ae959
Alan Poveda <alsepoveda@gmail.com>
2022-05-20 10:57:51 -0300

    Finish lab 09

--- lab009/scripts.js
+++ lab009/scripts.js
@@ -3,12 +3,14 @@ let contador = 0
 let res = document.querySelector('section#result')
 // As variáveis declaradas aqui fora são consideradas GLOBAIS e funcionam dentro do programa inteiro
 
+// Contador ++ para somar
 function contar() {
-    contador -- // É a mesma coisa que contador = contador + 1
+    contador ++ // É a mesma coisa que contador = contador + 1
     res.innerHTML = `<p>O contador está com <mark>${contador}</mark> cliques.</p>`
 }
 
+// Contador = 0 e html o mesmo de cima
 function zerar() {
-    contador
-    res.innerHTML = null
+    contador = 0
+    res.innerHTML = `<p>O contador está com <mark>${contador}</mark> cliques.</p>`
 }
